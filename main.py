81f82d105e3cfd4a17b2e255b99a0d82e8eb6ecb
starry-skies <mmazin2028@kehillahstudent.org>
2024-09-15 19:32:26 -0700

    done a01 exercise

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+
 import turtle
+numberofsides = 3
+
+
+def draw():
+         for i in range (3):
+                turtle.forward(100)
+                turtle.left(360/numberofsides)
+for i in range(3):
+        draw()
+        turtle.right(120)
+turtle.left(180)
+draw()
+for i in range (2):
+       turtle.right(120)
+       draw()
+    
+
+     
+    
+
+turtle.exitonclick()
 
